6b2b854fc78aadf8887ffe75f16f97a5a4815e69
Teemu Kakko <sormiteippi@windowslive.com>
2024-11-06 20:38:30 +0200

    Building Landing component

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,16 +1,20 @@
 import type { FC } from 'react';
 
-type Props = {
-  bgColor: string;
-};
+type Props = {};
 
-const Landing: FC<Props> = ({ bgColor }) => {
+const Landing: FC<Props> = () => {
   return (
-    <div style={{ backgroundColor: bgColor }} className='w-full h-lvh'>
-      <div className='inner-landing-wrapper'>
-        <h1 className='landing-title'>Hey,</h1>
-        <h1 className='landing-title'>I'm Teemu</h1>
-        <h2 className='landing-description'>a Finnish programmer journalist</h2>
+    <div className='relative h-lvh w-full bg-red-900'>
+      <div className='flex h-full w-full flex-col items-start justify-center'>
+        <h1 className='font-montserrat text-orange text-lg font-black leading-none mix-blend-screen'>
+          Hey,
+          <br />
+          I'm Teemu
+        </h1>
+
+        <p className='font-montserrat text-orange font-light leading-tight mix-blend-screen'>
+          a Finnish programmer journalist
+        </p>
       </div>
     </div>
   );
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  important: true,
+  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          ...defaultTheme.colors.green,
+          900: '#001e1e',
+        },
+        red: {
+          ...defaultTheme.colors.orange,
+          900: '#3f1e1e',
+        },
+        orange: '#fb544d',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+};
