6592df2a2344a53fcbdae9f0363ca65514b3144d
Teemu Kakko <sormiteippi@windowslive.com>
2024-11-06 19:33:44 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [react()],
+  base: '/film',
+  resolve: {
+    alias: {
+      '~': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
