a49adf22417001a0e82c53ed8529257c83c4c006
Teemu Kakko <sormiteippi@windowslive.com>
2024-11-06 20:46:02 +0200

    Minor styling

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -4,16 +4,16 @@ type Props = {};
 
 const Landing: FC<Props> = () => {
   return (
-    <div className='relative h-lvh w-full bg-red-900'>
+    <div className='relative h-lvh w-full bg-neutral-200'>
       <div className='flex h-full w-full flex-col items-start justify-center'>
-        <h1 className='font-montserrat text-orange text-lg font-black leading-none mix-blend-screen'>
+        <h1 className='font-montserrat whitespace-nowrap text-9xl font-black leading-none text-black'>
           Hey,
           <br />
           I'm Teemu
         </h1>
 
-        <p className='font-montserrat text-orange font-light leading-tight mix-blend-screen'>
-          a Finnish programmer journalist
+        <p className='font-montserrat text-3xl font-light leading-tight text-black'>
+          a Finnish journalist and developer
         </p>
       </div>
     </div>
